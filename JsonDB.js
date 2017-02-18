0a253b1cd972210196c2fb69e415e1538987a682
Dorgimon <dorgimon@foxmail.com>
2017-02-18 22:30:40 +0800

    Add files via upload

---
+++ JsonDB.js
@@ -0,0 +1,76 @@
+var JsonDB = '{"Config": [{"Name": "MyDB","Code": "UTF-8","Library": "./" }],"Users": [{"ID": "Root","Password": "Root","Jurisdiction": "ROOT"},{"ID": "Guest","Password": "Guest","Jurisdiction": "GUEST"}]}';
+
+function Add_Table(DB, Table) {
+    var OBJ = eval('(' + DB + ')');
+    var Arr = new Array();
+    OBJ[Table] = Arr;
+    return OBJ;
+}
+
+function Add_Table_Data(DB, Table) {
+    var OBJ = eval('(' + DB + ')');
+    DATA = new Object();
+    for (var Name in OBJ[Table][0]) {
+        DATA[Name] = "";
+    }
+    OBJ[Table].push(DATA);
+    return OBJ;
+}
+
+function Add_Field_Name(DB, Table, Name) {
+    var OBJ = eval('(' + DB + ')');
+    DATA = new Object();
+    DATA[Name] = "";
+    for (var i in OBJ[Table]) {
+        OBJ[Table][i][Name] = "";
+    }
+    return OBJ;
+}
+
+function Add_Field_Value(DB, Table, Order, Name, Value) {
+    var OBJ = eval('(' + DB + ')');
+    OBJ[Table][Order][Name] = Value;
+    return OBJ;
+}
+
+function Query_Value(DB, Table, Order, Name) {
+    var OBJ = eval('(' + DB + ')');
+    return OBJ[Table][Order][Name];
+}
+
+function Delete_Table(DB, Table) {
+    var OBJ = eval('(' + DB + ')');
+    delete OBJ[Table];
+    return OBJ;
+}
+
+function Delete_Table_Data(DB, Table, Order) {
+    var OBJ = eval('(' + DB + ')');
+    delete OBJ[Table][Order];
+    return OBJ;
+}
+
+function Delete_Table_Field(DB, Table, Name) {
+    var OBJ = eval('(' + DB + ')');
+    for (var i in OBJ[Table]) {
+        delete OBJ[Table][i][Name];
+    }
+    return OBJ;
+}
+
+console.log('Add_Table_/添加空表');
+console.log(Add_Table('{"Config": [{"Name": "MyDB","Code": "UTF-8","Library": "./" }],"Users": [{"ID": "Root","Password": "Root","Jurisdiction": "ROOT"},{"ID": "Guest","Password": "Guest","Jurisdiction": "GUEST"}]}', 'Table'));
+console.log('Add_Table_Data/添加空记录');
+console.log(Add_Table_Data('{"Config": [{"Name": "MyDB","Code": "UTF-8","Library": "./" }],"Users": [{"ID": "Root","Password": "Root","Jurisdiction": "ROOT"},{"ID": "Guest","Password": "Guest","Jurisdiction": "GUEST"}]}', 'Users'));
+console.log('Add_Field_Name/添加字段名');
+console.log(Add_Field_Name('{"Config": [{"Name": "MyDB","Code": "UTF-8","Library": "./" }],"Users": [{"ID": "Root","Password": "Root","Jurisdiction": "ROOT"},{"ID": "Guest","Password": "Guest","Jurisdiction": "GUEST"}]}', 'Users', 'Hello'));
+console.log('Add_Field_Value/添加字段值');
+console.log(Add_Field_Value('{"Config": [{"Name": "MyDB","Code": "UTF-8","Library": "./" }],"Users": [{"ID": "Root","Password": "Root","Jurisdiction": "ROOT"},{"ID": "Guest","Password": "Guest","Jurisdiction": "GUEST"}]}', 'Users', 0, 'Name', 'Value'));
+console.log('Query_Value/查询字段值');
+console.log(Query_Value('{"Config": [{"Name": "MyDB","Code": "UTF-8","Library": "./" }],"Users": [{"ID": "Root","Password": "Root","Jurisdiction": "ROOT"},{"ID": "Guest","Password": "Guest","Jurisdiction": "GUEST"}]}', "Users", 0, "ID"));
+console.log('Delete_Table/删除表');
+console.log(Delete_Table('{"Config": [{"Name": "MyDB","Code": "UTF-8","Library": "./" }],"Users": [{"ID": "Root","Password": "Root","Jurisdiction": "ROOT"},{"ID": "Guest","Password": "Guest","Jurisdiction": "GUEST"}]}', 'Table'));
+console.log('Delete_Table_Data/删除记录');
+console.log(Delete_Table_Data('{"Config": [{"Name": "MyDB","Code": "UTF-8","Library": "./" }],"Users": [{"ID": "Root","Password": "Root","Jurisdiction": "ROOT"},{"ID": "Guest","Password": "Guest","Jurisdiction": "GUEST"}]}', 'Users', 0));
+console.log('Delete_Table_Field/删除字段');
+console.log(Delete_Table_Field('{"Config": [{"Name": "MyDB","Code": "UTF-8","Library": "./" }],"Users": [{"ID": "Root","Password": "Root","Jurisdiction": "ROOT"},{"ID": "Guest","Password": "Guest","Jurisdiction": "GUEST"}]}', 'Users', 'Password'));
